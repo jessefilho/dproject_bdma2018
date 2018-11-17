ece3465543a196e3a673487efa9ae6febda3a5ce
Jesse Ferreira Filho <jessefilhojfnf@gmail.com>
2018-11-17 03:34:01 +0100

    add python script that extract artist features

--- ETL/scripts/extractartist_spotifymusicbrainz.py
+++ ETL/scripts/extractartist_spotifymusicbrainz.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# In[66]:
-#python -m pip install 
-
+#python -m pip install xlsxwriter
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
@@ -11,84 +8,156 @@ import time
 from openpyxl import load_workbook
 import os.path 
 import psycopg2
+import xlsxwriter
 #import pgdb
 
 hostname = '10.195.25.10'
-username = 'postgres'
-password = '123'
+username = 'userbkp'
+password = 'goma123'
 database = 'musicbrainz'
 port='54587'
 cid ="a154f4842df643a99f9057fb741c86e0" 
 secret = "543080531fce4f30be3da2c36782ace1"
 
+today_date=(time.strftime("%d_%m_%Y  %M_%S"))
+#date=(time.strftime("%d/%m/%Y"))
+filepath = 'F:\Documents\dproject_bdma2018\DATA'+today_date+'.xlsx'
+
+
+    
+    
+
+
+query = """SELECT distinct artist_table.name_group,
+              artist_table.id as id_msbz,
+              artist_table.position as id_member_position,
+              artist_table.type as id_type_group, 
+              artist_table.area as id_country_origin, 
+              area_table.countrycity_name as country_origin,
+              artist_table.begin_area as id_city_origin ,
+              (SELECT musicbrainz.area.name FROM musicbrainz.area WHERE musicbrainz.area.id = artist_table.begin_area AND musicbrainz.area.type = 3) as city_origin,
+              gender_table.name as gender_sex
+
+              FROM (SELECT musicbrainz.artist.name as name_group,* 
+                FROM musicbrainz.artist
+                INNER JOIN musicbrainz.artist_credit_name
+              ON musicbrainz.artist.id = musicbrainz.artist_credit_name.artist) as artist_table
+              INNER JOIN musicbrainz.gender as gender_table
+              ON artist_table.gender = gender_table.id
+              INNER JOIN (select musicbrainz.area.id as id_area, musicbrainz.area.name as countrycity_name, musicbrainz.area_type.name as countrycity_type
+                    FROM musicbrainz.area
+                    INNER JOIN musicbrainz.area_type
+                    on musicbrainz.area.type = musicbrainz.area_type.id
+                    INNER JOIN (SELECT musicbrainz.area_alias.area as id_area FROM musicbrainz.area_alias WHERE musicbrainz.area_alias.locale = 'en') as area_alias_table
+                    on musicbrainz.area.id = area_alias_table.id_area
+                    where musicbrainz.area_type.id = 1) as area_table
+              ON artist_table.area = area_table.id_area
+
+              WHERE artist_table.area is not null AND artist_table.begin_area is not null AND artist_table.area != artist_table.begin_area"""
+
 client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-today_date=(time.strftime("%d_%m_%Y"))
-date=(time.strftime("%d/%m/%Y"))
+
 
 def doQuery( conn ) :
     
     cur = conn.cursor()
-    cur.execute( "SELECT  name , 'x' FROM musicbrainz.artist where area = 73 group by name limit 10" )
+    cur.execute(query)
     return cur.fetchall()
  
     
 artist_musicbrainz =[]
 myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname=database,port=port )
-artist_musicbrainz =doQuery( myConnection )
+artist_musicbrainz = doQuery( myConnection )
 myConnection.close()
 # print size list
 
 
 
 #Verify if File in certain date already exists
-filename='F:\Documents\dproject_bdma2018\DATA'+today_date+'.xlsx'
+filename=filepath
 if os.path.exists(filename):
     print(filename+' '+'exists')
+    print("Extraction is starts...")
 else: 
     # create empty lists where the results are going to be stored
-    artist_name = []
-    artist_genre= []
+    artist_name_group = []
+    artist_id_msbz = []
+    artist_id_member_position = []
+    artist_id_type_group = []
+    artist_id_country_origin =[]
+    artist_country_origin =[]
+    artist_id_city_origin = []
+    artist_city_origin = []
+    artist_gender_sex = []
+    #spotify attributes
     artist_popularity = []
-    artist_id = []
+    artist_id_spotify = []
     artist_followers = []
-    artist_x = []
- 
-    for i in range(0,len(artist_musicbrainz)-1,1):
-        for name,x in artist_musicbrainz :
-            track_results = sp.search(q=name, type='artist',market='FR', limit=50)
-           # print (track_results)
-            
-            if  not track_results[0]['items'] :
-                #print(track_results[t]['items'])
-                print('artiste non trouvé')
-        
-            else : 
-                #print (track_results)
-                for i, t in enumerate(track_results):
-                    print (track_results[t]['items'][0]['name'])                    
-                    artist_name.append(track_results[t]['items'][0]['name'])
-                    artist_genre.append(track_results[t]['items'][0]['genres'])
-                    artist_popularity.append(track_results[t]['items'][0]['popularity'])
-                    artist_id.append(track_results[t]['items'][0]['id'])
-                    artist_followers.append(track_results[t]['items'][0]['followers']['total'])
-                    artist_x.append(x)
-                    
-                    
-                    print ('finish')
-                print ('finish for')
-                df_tracks = pd.DataFrame({'artist_name':artist_name,
-                                          'artist_genre':artist_genre,
-                                          'artist_popularity':artist_popularity,
-                                          'artist_id.':artist_id,
-                                          'Date':date,
-                                          'artist_followers':artist_followers,
-                                          'x': artist_x
-                                         })
+    artist_type = []
+    artist_genre = []
+    artist_gender = []
 
-  
-                  #Create xlsx File 
+ 
+
+    for i in range(0,len(artist_musicbrainz)-1,1):
+        for name_group,id_msbz,id_member_position,id_type_group,id_country_origin,country_origin,id_city_origin,city_origin,gender_sex in artist_musicbrainz :
+            track_results = sp.search(q=name_group, type='artist',market='FR', limit=50)
+            #print (track_results)
+            for i, t in enumerate(track_results):
+              if  not track_results[t]['items']:
+                  #print(track_results[t]['items'])
+                  print('artiste non trouvé')
+              else : 
+                  print (track_results[t]['items'][0]['name'])                    
+                  artist_name_group.append(track_results[t]['items'][0]['name'])
+                  artist_id_msbz.append(id_msbz)
+                  artist_id_member_position.append(id_member_position)
+                  artist_id_type_group.append(id_type_group)
+                  artist_id_country_origin.append(id_country_origin)
+                  artist_country_origin.append(country_origin)
+                  artist_id_city_origin.append(id_city_origin)
+                  artist_city_origin.append(city_origin)
+                  artist_gender_sex.append(gender_sex)
+                  #spotify attributes
+                  artist_popularity.append(track_results[t]['items'][0]['popularity'])
+                  artist_id_spotify.append(track_results[t]['items'][0]['id'])
+                  artist_followers.append(track_results[t]['items'][0]['followers']['total'])
+                  artist_genre.append(track_results[t]['items'][0]['genres'])
+                                 
+                  
+                  print ('finish ->')
+              print ('finish for exited')
+              df_tracks = pd.DataFrame({'artist_name_group':artist_name_group,
+                                        'artist_id_msbz':artist_id_msbz,
+                                        'artist_id_member_position':artist_id_member_position,
+                                        'artist_id_type_group':artist_id_type_group,
+                                        'artist_id_country_origin':artist_id_country_origin,
+                                        'artist_country_origin':artist_country_origin,
+                                        'artist_id_city_origin':artist_id_city_origin,
+                                        'artist_city_origin':artist_city_origin,
+                                        'artist_gender_sex':artist_gender_sex,
+                                        'artist_popularity':artist_popularity,
+                                        'artist_id_spotify':artist_id_spotify,
+                                        'artist_followers':artist_followers,
+                                        'artist_genre':artist_genre
+                                       })
+
+
+              #Create xlsx File 
+              try:
+                # myConnection = psycopg2.connect( host=hostname, user=username, password=password, dbname='stages',port=port )
+                # cur = conn.cursor()
+                # cur.execute("INSERT INTO public.stage_artist (column1, column2, column3 .....)",)
+                # myConnection.commit()
+                # myConnection.close()
+
+
                 writer = pd.ExcelWriter(filename, engine='xlsxwriter')
                 df_tracks.to_excel(writer,encoding='utf8')
                 writer.save()
+                print "Inserted!"
+              except:
+                print " ###IGNORED#### UnicodeDecodeError: 'ascii' codec can't decode byte 0xc3!"
+
